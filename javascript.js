e132711f080aaf9c51ad5365e5a3fc76db82dc05
Francirafael <francir4@gmail.com>
2022-01-15 12:29:02 -0300

    javascript

--- javascript.js
+++ javascript.js
@@ -1,26 +1,38 @@
-const imagem = document. querySelector('img');
-const botao = document.  querySelector('button');
+const imagem = document.querySelector('img');
+const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
-const Condicao = document. querySelector('#especie');
-const especies = document. queryselector('#status');
+const especie = document.querySelector('#specie');
+const condicao = document.querySelector('#status');
+
+traduzirCondicao = (data) => {
+    if(data.status == 'unknown'){
+        return 'Não sabemos';
+    }else if(data.status == 'Alive'){
+        return 'Sim';
+    }else {
+        return 'Não. Está morto';
+    }
+}
+
+gerarValorAletorio = () => {
+    return Math.floor(Math.random() * 671);
+}
 
 pegarPersonagem = () => {
-
-    return fetch(`https://rickandmortyapi.com/api/character/8`, {
-        method: 'GET',
+    let numeroAleatorio = gerarValorAletorio();
+    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+        method:'GET',
         headers: {
-         Accept: 'application/json',
-         "Content-type": 'application/json'
+            Accept: 'application/json',
+            "Content-type": 'application/json'
         }
-}).then((response) => response.json()).then((data) =>{
-    imagem.src = data.image;
-    imagem.alt = data.name;
-    nomeDoPersonagem.innerHTML = data.name;
-    Condicao.innerHTML = data.status;
-    condicao.innerHTML = traduzirCondicao(data);
-    
-});
-
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = traduzirCondicao(data);
+    });
 }
 
 botao.onclick = pegarPersonagem;
