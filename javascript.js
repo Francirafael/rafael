3db909caa9a42abd48c7e390965efcf7eaa771e6
Francirafael <francir4@gmail.com>
2022-01-15 02:11:55 -0300

    atualição pgm web

--- javascript.js
+++ javascript.js
@@ -1,5 +1,22 @@
 const imagem = document. querySelector("img");
-const botao = document.  querySelector("button");
+const botao = document.  querySelector("botao");
 const nomeDoPersonagem = document.querySelector("#nome");
 const status = document. querySelector("#status");
 const especie = document. queryselector("#especie");
+
+pegarPersonagem =() => {
+
+    return fetch(`http://rickandmortyapi.com/api/character/2`, {
+        method: 'GET',
+        headers: {
+         Accept: 'application/json',
+         "Content-type": 'application/json'
+        }
+}).then((Response) => Response.json()).then((data) =>{
+    imagem.src = data.image;
+    imagem.alt = data.name;
+});
+
+}
+
+botao.onclick =pegarPersonagem
