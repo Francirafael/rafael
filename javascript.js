8d8c909b95871c725088f4a9f42c35202f2d2c95
Francirafael <francir4@gmail.com>
2022-01-15 00:43:42 -0300

    Capturar elementos html manipulaveis

---
+++ javascript.js
@@ -0,0 +1,5 @@
+const imagem = document. querySelector("img");
+const botao = document.  querySelector("button");
+const nomeDoPersonagem = document.querySelector("#nome");
+const status = document. querySelector("#status");
+const especie = document. queryselector("#especie");
